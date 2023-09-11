6702045599b934e44bd0a366ded616e4787d42dc
winstonl <wrlucasas@gmail.com>
2023-09-11 01:59:58 -0700

    added icon growing when hovered over on footer icons

---
+++ app.js
@@ -0,0 +1,29 @@
+const spotBtn = document.querySelector('.spotBtn');
+
+spotBtn.addEventListener('mouseover', (height) => {
+    spotBtn.style.height = "20px";
+})
+
+spotBtn.addEventListener('mouseout', (stop) => {
+    spotBtn.style.height = "16px";
+})
+
+const instaBtn = document.querySelector('.instaBtn');
+
+instaBtn.addEventListener('mouseover', (height) => {
+    instaBtn.style.height = "20px";
+})
+
+instaBtn.addEventListener('mouseout', (stop) => {
+    instaBtn.style.height = "16px";
+})
+
+const linkedBtn = document.querySelector('.linkInBtn');
+
+linkedBtn.addEventListener('mouseover', (height) => {
+   linkedBtn.style.height = "20px";
+})
+
+linkedBtn.addEventListener('mouseout', (stop) => {
+    linkedBtn.style.height = "16px";
+})
